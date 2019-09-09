1be2d59ffb69a121f1eea2f9d84298c5343f8155
Adrian <adrianivsdk@gmail.com>
2019-09-09 16:53:50 -0400

    Added the b boyz object

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 //BBOYZ WERE HEREZ NO HAX PLS EVIL BRITO N NIKO
 alert("what's the password?")
+console.log('i love the b boyz')
+var bBooyz = {
+    awesome:true,
+    beautiful:true,
+    beautyLevel:10,
+    smashBrosTalent:10,
+}
